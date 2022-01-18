3ee8a38c04fab4e908a89e972904ef48f27b1bd7
Alan <tapia.alan.g@gmail.com>
2022-01-17 21:05:37 -0300

    info-cards

---
+++ src/components/info-cards/InfoCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./info-card.scss";
+
+export default function InfoCard(props) {
+  const { img, title, description } = props;
+  return (
+    <div className="card">
+      <div className="icon">
+        <img src={img} />
+      </div>
+      <div className="info">
+        <h4>{title}</h4>
+        <h5>{description}</h5>
+      </div>
+    </div>
+  );
+}
