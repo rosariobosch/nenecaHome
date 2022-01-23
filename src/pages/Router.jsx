9c76f7beb7292a1b24a196972161a8c3249eb458
Rosario Bosch <rosariobosch93@gmail.com>
2022-01-23 17:46:22 -0300

    faqs

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutUs from "../components/sobre-nosotros/AboutUs";
-import Footer from "../components/footer/Footer";
+// import AboutUs from "../components/sobre-nosotros/AboutUs";
+// import Footer from "../components/footer/Footer";
+import Faqs from "../components/faqs/faqs";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/nosotras" element={<AboutUs />} />
-        <Route exact path="*" element={<Footer />} />
+        {/* <Route exact path="/nosotras" element={<AboutUs />} /> */}
+        <Route exact path="/FAQs" element={<Faqs />} />
+        {/* <Route exact path="*" element={<Footer />} /> */}
       </Routes>
     </BrowserRouter>
   );
